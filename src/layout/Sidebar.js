9896ff29d18e4b9e53db19d84bcd98efc8066e42
zhanghuan123 <zhhu_123@163.com>
2017-12-05 12:32:05 +0800

    chore(*):reset SideBar

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,87 +1,81 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import compose from 'recompose/compose';
-import Drawer from 'material-ui/Drawer';
-import Paper from 'material-ui/Paper';
-import muiThemeable from 'material-ui/styles/muiThemeable';
-import { setSidebarVisibility as setSidebarVisibilityAction } from '../actions';
+import { withStyles } from 'material-ui/styles';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Collapse from 'material-ui/transitions/Collapse';
+import ExpandLess from 'material-ui-icons/ExpandLess';
+import ExpandMore from 'material-ui-icons/ExpandMore';
+import logo from '../assets/images/logo.svg';
+import sides from '../api/side.json';
 
-const getWidth = width => (typeof width === 'number' ? `${width}px` : width);
-
-const getStyles = ({ drawer }) => {
-    const width = drawer && drawer.width ? getWidth(drawer.width) : '16em';
-
-    return {
-        sidebarOpen: {
-            flex: `0 0 ${width}`,
-            marginLeft: 0,
-            order: -1,
-            transition: 'margin 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-        },
-        sidebarClosed: {
-            flex: `0 0 ${width}`,
-            marginLeft: `-${width}`,
-            order: -1,
-            transition: 'margin 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-        },
-    };
-};
+const styles = theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        background: theme.palette.background.paper,
+    },
+    nested: {
+        paddingLeft: theme.spacing.unit * 4,
+    },
+});
 
 // We shouldn't need PureComponent here as it's connected
 // but for some reason it keeps rendering even though mapStateToProps returns the same object
 class Sidebar extends PureComponent {
-    handleClose = () => {
-        this.props.setSidebarVisibility(false);
+    constructor(props) {
+        super(props);
+        this.state = {
+            navs: sides,
+        };
+    }
+    handleClick(index) {
+        const self = this;
+        Object.keys(this.state.navs).forEach(item => {
+            if (item === index.toString()) {
+                self.state.navs[item].open = !self.state.navs[item].open;
+            } else {
+                self.state.navs[item].open = false;
+            }
+        });
+        self.setState({ sides });
     };
-
     render() {
-        const { open, setSidebarVisibility, children, muiTheme } = this.props;
-        const styles = getStyles(muiTheme);
-
         return (
-            <Responsive
-                small={
-                    <Drawer
-                        docked={false}
-                        open={open}
-                        onRequestChange={setSidebarVisibility}
-                    >
-                        {React.cloneElement(children, {
-                            onMenuTap: this.handleClose,
-                        })}
-                    </Drawer>
+            <div className='sideBar'>
+                <div className='logo'>
+                    <img src={ logo } alt=''/>
+                </div>
+                {
+                    this.state.navs.map((item, index) => {
+                        return (
+                            <List className={ this.props.classes.root } key={ index }>
+                                <ListItem button onClick={ () => this.handleClick(index) }>
+                                    <ListItemText inset primary={ item.name }/>
+                                    {item.open ? <ExpandLess/> : <ExpandMore/>}
+                                </ListItem>
+                                <Collapse component='li' in={ item.open } transitionDuration='auto' unmountOnExit>
+                                    <List disablePadding>
+                                        {
+                                            item.children.map((child, childIndex) => {
+                                                return (
+                                                    <ListItem button key={ index.toString() + childIndex }>
+                                                        <ListItemText inset primary={ child.name }/>
+                                                    </ListItem>
+                                                );
+                                            })
+                                        }
+                                    </List>
+                                </Collapse>
+                            </List>
+                        );
+                    })
                 }
-                medium={
-                    <Paper
-                        style={open ? styles.sidebarOpen : styles.sidebarClosed}
-                    >
-                        {React.cloneElement(children, {
-                            onMenuTap: () => null,
-                        })}
-                    </Paper>
-                }
-            />
+            </div>
         );
     }
 }
 
 Sidebar.propTypes = {
-    children: PropTypes.node.isRequired,
-    muiTheme: PropTypes.object.isRequired,
-    open: PropTypes.bool.isRequired,
-    setSidebarVisibility: PropTypes.func.isRequired,
+    classes: PropTypes.object.isRequired,
 };
-
-const mapStateToProps = (state, props) => ({
-    open: state.admin.ui.sidebarOpen,
-    locale: state.locale, // force redraw on locale change
-    theme: props.theme, // force redraw on theme changes
-});
-
-export default compose(
-    muiThemeable(),
-    connect(mapStateToProps, {
-        setSidebarVisibility: setSidebarVisibilityAction,
-    })
-)(Sidebar);
+export default withStyles(styles)(Sidebar);
