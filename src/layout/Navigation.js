19802f2f8d287a050a6969e563d675aaaf74bf09
zhanghuan123 <zhhu_123@163.com>
2017-12-07 23:56:31 +0800

    chore(layout): Navigation

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -1,2 +1,26 @@
-import default from "material-ui/Modal/Backdrop";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import MuiAppBar from 'material-ui/AppBar';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+import compose from 'recompose/compose';
+import { toggleSidebar as toggleSidebarAction } from '../../actions';
 
+const Navigation = ({ title, toggleSidebar }) => (
+    <MuiAppBar title={ title } onLeftIconButtonTouchTap={ toggleSidebar }/>
+);
+
+Navigation.propTypes = {
+    title: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
+        .isRequired,
+    toggleSidebar: PropTypes.func.isRequired,
+};
+
+const enhance = compose(
+    muiThemeable(), // force redraw on theme change
+    connect(null, {
+        toggleSidebar: toggleSidebarAction,
+    })
+);
+
+export default enhance(Navigation);
