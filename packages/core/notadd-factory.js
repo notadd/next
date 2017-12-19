5a049e0c0d93c61b19eb8dab688371bad0235e5b
twilroad <269044570@qq.com>
2017-12-19 19:17:35 +0800

    fix(publish): fix error for publish

--- packages/core/notadd-factory.js
+++ packages/core/notadd-factory.js
@@ -10,6 +10,14 @@ class NotaddFactoryStatic extends core_1.NestFactoryStatic {
      * @returns { Promise<INestApplication> }
      */
     async create(module, express = express_adapter_1.ExpressAdapter.create()) {
+        console.log(`
+                         _            _     _
+             _ __   ___ | |_ __ _  __| | __| |
+            | '_ \\ / _ \\| __/ _\` |/ _\` |/ _\` |
+            | | | | (_) | || (_| | (_| | (_| |
+            |_| |_|\\___/ \\__\\__,_|\\__,_|\\__,_|
+
+        `);
         return super.create(module, express);
     }
 }
