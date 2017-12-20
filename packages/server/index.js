9bbc25890902e9b817981eac4bc68c587e69d745
twilroad <269044570@qq.com>
2017-12-20 19:02:40 +0800

    feat(document): add document with swagger

--- packages/server/index.js
+++ packages/server/index.js
@@ -4,11 +4,32 @@ const core_1 = require("@notadd/core");
 const application_module_1 = require("./modules/application.module");
 const common_1 = require("@nestjs/common");
 const ip = require("ip");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const port = 3000;
-    const address = 'http://' + ip.address() + ':' + port;
+    const address = `http://${ip.address()}:${port}`;
+    /**
+     * @type { INestApplication }
+     */
     const application = await core_1.NotaddFactory.create(application_module_1.ApplicationModule);
+    /**
+     * @type { Logger }
+     */
     const logger = new common_1.Logger('NotaddFactory', true);
+    /**
+     * @type { SwaggerBaseConfig }
+     */
+    const options = new swagger_1.DocumentBuilder()
+        .setTitle('Notadd')
+        .setDescription('API document for Notadd.')
+        .setVersion('2.0')
+        .addBearerAuth()
+        .build();
+    /**
+     * @type {SwaggerDocument}
+     */
+    const document = swagger_1.SwaggerModule.createDocument(application, options);
+    swagger_1.SwaggerModule.setup('/api-doc', application, document);
     await application.listen(port, () => {
         logger.log(`Server on: ${address}`);
     });
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,12 +2,35 @@ import { NotaddFactory } from '@notadd/core';
 import { ApplicationModule } from './modules/application.module';
 import { Logger } from "@nestjs/common";
 import * as ip from "ip";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 export async function bootstrap() {
     const port = 3000;
-    const address = 'http://' + ip.address() + ':' + port;
+    const address = `http://${ip.address()}:${port}`;
+    /**
+     * @type { INestApplication }
+     */
     const application = await NotaddFactory.create(ApplicationModule);
+    /**
+     * @type { Logger }
+     */
     const logger = new Logger('NotaddFactory', true);
+    /**
+     * @type { SwaggerBaseConfig }
+     */
+    const options = new DocumentBuilder()
+        .setTitle('Notadd')
+        .setDescription('API document for Notadd.')
+        .setVersion('2.0')
+        .addBearerAuth()
+        .build();
+    /**
+     * @type {SwaggerDocument}
+     */
+    const document = SwaggerModule.createDocument(application, options);
+
+    SwaggerModule.setup('/api-doc', application, document);
+
     await application.listen(port, () => {
         logger.log(`Server on: ${address}`);
     });
