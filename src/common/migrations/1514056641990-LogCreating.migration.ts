bf0e4f13295b7b154f06aa4faaf7e820e9e33cb2
twilroad <269044570@qq.com>
2017-12-24 22:56:12 +0800

    feat(migration): add migration definition for table log and setting

---
+++ src/common/migrations/1514056641990-LogCreating.migration.ts
@@ -0,0 +1,10 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class LogCreating1514056641990 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+}
