b3f79715e4f6ebc4fbd74363fc8126cc60613495
zhanghuan123 <zhhu_123@163.com>
2018-01-04 12:30:25 +0800

    chore(layout): Add Drawer

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -18,10 +18,20 @@ import AddonOpen from './AddonOpen';
 import AddonImport from './AddonImport';
 import AddonInstall from './AddonInstall';
 import { HashRouter } from 'react-router-dom';
+import Drawer from 'material-ui/Drawer';
+import Hidden from 'material-ui/Hidden';
 
 type Props = RouteProps;
 
 class App extends React.Component<Props, {}> {
+    state = {
+        open: true,
+    };
+    toggleDrawer = () => {
+        this.setState({
+            open: !open,
+        });
+    };
     render() {
         return (
             <HashRouter  basename="/">
@@ -34,24 +44,44 @@ class App extends React.Component<Props, {}> {
                             return (
                                 <div className="main-view">
                                     <Header/>
-                                    <div className="right-view">
-                                        <Side/>
-                                        <div className="view">
-                                            <Route exact path="/home" component={Home}/>
-                                            <Route exact path="/configurations" component={Configurations}/>
-                                            <Route exact path="/seo" component={Seo}/>
-                                            <Route exact path="/upload" component={Upload}/>
-                                            <Route exact path="/mail" component={Mail}/>
-                                            <Route exact path="/debug" component={Debug}/>
-                                            <Route exact path="/extension" component={Extension}/>
-                                            <Route exact path="/module/open-module" component={ModuleOpen}/>
-                                            <Route exact path="/module/domain-config" component={ModuleDomain}/>
-                                            <Route exact path="/module/import-export" component={ModuleImport}/>
-                                            <Route exact path="/module/install" component={ModuleInstall}/>
-                                            <Route exact path="/addon/openAddon" component={AddonOpen}/>
-                                            <Route exact path="/addon/import-export" component={AddonImport}/>
-                                            <Route exact path="/addon/install" component={AddonInstall}/>
-                                        </div>
+                                    <Hidden mdUp>
+                                        <Drawer
+                                            type="temporary"
+                                            open={this.state.open}
+                                            onClose={this.toggleDrawer}
+                                            classes={{
+                                                paper: classes.drawerPaper,
+                                            }}
+                                            ModalProps={{
+                                                keepMounted: true, // Better open performance on mobile.
+                                            }}
+                                        >
+                                            <Side/>
+                                        </Drawer>
+                                    </Hidden>
+                                    <Hidden smDown implementation="css">
+                                        <Drawer
+                                            type="permanent"
+                                            open={this.state.open}
+                                        >
+                                            <Side/>
+                                        </Drawer>
+                                    </Hidden>
+                                    <div className="view">
+                                        <Route exact path="/home" component={Home}/>
+                                        <Route exact path="/configurations" component={Configurations}/>
+                                        <Route exact path="/seo" component={Seo}/>
+                                        <Route exact path="/upload" component={Upload}/>
+                                        <Route exact path="/mail" component={Mail}/>
+                                        <Route exact path="/debug" component={Debug}/>
+                                        <Route exact path="/extension" component={Extension}/>
+                                        <Route exact path="/module/open-module" component={ModuleOpen}/>
+                                        <Route exact path="/module/domain-config" component={ModuleDomain}/>
+                                        <Route exact path="/module/import-export" component={ModuleImport}/>
+                                        <Route exact path="/module/install" component={ModuleInstall}/>
+                                        <Route exact path="/addon/openAddon" component={AddonOpen}/>
+                                        <Route exact path="/addon/import-export" component={AddonImport}/>
+                                        <Route exact path="/addon/install" component={AddonInstall}/>
                                     </div>
                                 </div>
                             );
