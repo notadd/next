4a9d9845a983cb4aed8d6af1dc586e64480dd226
twilroad <269044570@qq.com>
2018-01-04 19:47:55 +0800

    feat(swagger): add swagger description for auth router

--- packages/common/controllers/auth.controller.js
+++ packages/common/controllers/auth.controller.js
@@ -8,6 +8,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -19,11 +22,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const auth_service_1 = require("../services/auth.service");
+const swagger_1 = require("@nestjs/swagger");
+const auth_login_dto_1 = require("../dtos/auth.login.dto");
 let AuthController = class AuthController {
     constructor(authService) {
         this.authService = authService;
     }
-    getToken() {
+    getToken(userInfo) {
         return __awaiter(this, void 0, void 0, function* () {
             return yield this.authService.createToken();
         });
@@ -35,10 +40,21 @@ let AuthController = class AuthController {
     }
 };
 __decorate([
+    swagger_1.ApiOperation({
+        title: 'JWT Login API',
+    }),
+    swagger_1.ApiResponse({
+        status: 201,
+        description: 'User info validate correct, create a token for this user.',
+    }),
+    swagger_1.ApiResponse({
+        status: 403,
+        description: 'User info validate error.'
+    }),
     common_1.Post('token'),
-    common_1.HttpCode(common_1.HttpStatus.OK),
+    __param(0, common_1.Body()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", []),
+    __metadata("design:paramtypes", [auth_login_dto_1.AuthLogin]),
     __metadata("design:returntype", Promise)
 ], AuthController.prototype, "getToken", null);
 __decorate([
@@ -48,6 +64,7 @@ __decorate([
     __metadata("design:returntype", Promise)
 ], AuthController.prototype, "authorized", null);
 AuthController = __decorate([
+    swagger_1.ApiUseTags('jwt'),
     common_1.Controller('auth'),
     __metadata("design:paramtypes", [auth_service_1.AuthService])
 ], AuthController);
--- src/common/controllers/auth.controller.ts
+++ src/common/controllers/auth.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Post, HttpStatus, HttpCode, Get } from '@nestjs/common';
+import { Controller, Post, HttpStatus, HttpCode, Get, Body } from '@nestjs/common';
 import { AuthService } from "../services/auth.service";
+import { ApiOperation, ApiResponse, ApiUseTags } from "@nestjs/swagger";
+import { AuthLogin } from "../dtos/auth.login.dto";
 
+@ApiUseTags('jwt')
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    /**
+     * @param { AuthService } authService
+     */
+    constructor(private readonly authService: AuthService) {
+    }
 
+    @ApiOperation({
+        title: 'JWT Login API',
+    })
+    @ApiResponse({
+        status: 201,
+        description: 'User info validate correct, create a token for this user.',
+    })
+    @ApiResponse({
+        status: 403,
+        description: 'User info validate error.'
+    })
     @Post('token')
-    @HttpCode(HttpStatus.OK)
-    public async getToken() {
+    public async getToken(@Body() userInfo: AuthLogin) {
         return await this.authService.createToken();
     }
 
