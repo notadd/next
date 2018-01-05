4996935148fb97523cf8bffd528bf625eb24f8cb
twilroad <269044570@qq.com>
2018-01-05 19:09:56 +0800

    refactor(type): add return type for user resolver

--- src/user/resolvers/user.resolver.ts
+++ src/user/resolvers/user.resolver.ts
@@ -1,12 +1,13 @@
 import { Query, Resolver } from "@nestjs/graphql";
 import { UserService } from "@notadd/user/services/user.service";
+import { User } from "../entities/user.entity";
 
 @Resolver('User')
 export class UserResolver {
     constructor(private readonly userService: UserService) {}
 
     @Query()
-    async getUsers() {
+    async getUsers(): Promise<User[]> {
         return await this.userService.findAll()
     }
 }
