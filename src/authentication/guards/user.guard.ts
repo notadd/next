313768dcf51d9736e52290384d5a49af4675be8c
twilroad <269044570@qq.com>
2018-01-12 16:49:32 +0800

    feat(guard): add guard for user

---
+++ src/authentication/guards/user.guard.ts
@@ -0,0 +1,8 @@
+import { CanActivate, ExecutionContext, Guard } from "@nestjs/common";
+
+@Guard()
+export class UserGuard implements CanActivate {
+    canActivate(request: any, context: ExecutionContext): boolean {
+        return true;
+    }
+}
