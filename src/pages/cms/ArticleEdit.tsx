84c1242e0ce251400cef1dcad0757f2f56d42c0a
tianjign <tfanxi@163.com>
2018-01-15 17:07:30 +0800

    chore: add article edit page content

--- src/pages/cms/ArticleEdit.tsx
+++ src/pages/cms/ArticleEdit.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import withStyles, { WithStyles } from 'material-ui/styles/withStyles';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
-import { FormControl } from 'material-ui/Form';
+import { FormControlLabel, FormControl } from 'material-ui/Form';
 import Input, { InputLabel } from 'material-ui/Input';
+import Switch from 'material-ui/Switch';
+import Button from 'material-ui/Button';
 
 const styles = {
     root: {
         'padding': '40px 30px',
+        'margin-bottom': '60px',
     },
     container: {
         display: 'flex',
@@ -25,12 +28,18 @@ const styles = {
         'font-size': '16px',
     },
     formControlMargin: {
-        'margin-bottom': '34px',
+        'margin-bottom': '32px',
     },
     underline: {
         '&:before': {
             background: '#dfdfdf',
         }
+    },
+    switchHeight: {
+        'height': '20px',
+    },
+    switchDefault: {
+        'height': 'inherit',
     },
 };
 type State = {
@@ -41,6 +50,12 @@ class ArticleEdit extends React.Component<WithStyles<keyof typeof styles>, State
         webName: 'NotAdd',
         img: 'LOGO.png',
         type: '新闻资讯',
+        abstract: '',
+        time: '2016-12-30',
+        link: 'http://',
+        origin: 'www.ibenchu.com',
+        kind: '新闻资讯',
+        isHidden: false,
     };
     handleChange = (name: any) => (event: any) => {
         let val = event.target.value;
@@ -56,68 +71,185 @@ class ArticleEdit extends React.Component<WithStyles<keyof typeof styles>, State
                 </p>
                 <h4 className="title">编辑</h4>
                 <Paper className={this.props.classes.root}>
-                    <Grid container spacing={24}>
-                        <Grid item xs={12} sm={6}>
-                            <FormControl fullWidth
-                                         className={this.props.classes.formControlMargin}>
-                                <InputLabel
-                                    htmlFor="name-simple"
-                                    className={this.props.classes.formLabelFont}
-                                >
-                                    文章标题
-                                </InputLabel>
-                                <Input
-                                    id="name-simple"
-                                    className={this.props.classes.formLabelFont}
-                                    classes={{
-                                        underline: this.props.classes.underline,
-                                    }}
-                                    onChange={this.handleChange('webName')}
-                                    value={this.state.webName}
-                                />
-                            </FormControl>
-                            <div className="editor">
-                                编辑器插件
-                            </div>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <FormControl fullWidth
-                                         className={this.props.classes.formControlMargin}>
-                                <InputLabel
-                                    htmlFor="name-simple"
-                                    className={this.props.classes.formLabelFont}
-                                >
-                                    缩略图
-                                </InputLabel>
-                                <Input
-                                    id="name-simple"
-                                    className={this.props.classes.formLabelFont}
-                                    classes={{
-                                        underline: this.props.classes.underline,
-                                    }}
-                                    onChange={this.handleChange('img')}
-                                    value={this.state.img}
-                                />
-                            </FormControl>
-                            <FormControl fullWidth>
-                                <InputLabel
-                                    htmlFor="name-simple"
-                                    className={this.props.classes.formLabelFont}
-                                >
-                                    分类
-                                </InputLabel>
-                                <Input
-                                    id="name-simple"
-                                    className={this.props.classes.formLabelFont}
-                                    classes={{
+                    <form className={this.props.classes.container} noValidate autoComplete="off">
+                        <Grid container spacing={24}>
+                            <Grid item xs={12} sm={6}>
+                                <FormControl fullWidth
+                                             className={this.props.classes.formControlMargin}>
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        文章标题
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
                                             underline: this.props.classes.underline,
                                         }}
-                                    onChange={this.handleChange('type')}
-                                    value={this.state.type}
+                                        onChange={this.handleChange('webName')}
+                                        value={this.state.webName}
+                                    />
+                                </FormControl>
+                                <div className="editor">
+                                    编辑器插件
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormControl fullWidth
+                                             className={this.props.classes.formControlMargin}>
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        缩略图
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('img')}
+                                        value={this.state.img}
+                                    />
+                                </FormControl>
+                                <FormControl fullWidth
+                                             className={this.props.classes.formControlMargin}>
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        分类
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('type')}
+                                        value={this.state.type}
+                                    />
+                                </FormControl>
+                                <FormControl fullWidth
+                                             className={this.props.classes.formControlMargin}>
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        摘要
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        multiline={true}
+                                        rowsMax="3"
+                                        rows="3"
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        className={this.props.classes.formLabelFont}
+                                        onChange={this.handleChange('abstract')}
+                                        value={this.state.abstract}
+                                    />
+                                </FormControl>
+                                <FormControl fullWidth
+                                             className={this.props.classes.formControlMargin}>
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        分类
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('kind')}
+                                        value={this.state.kind}
+                                    />
+                                </FormControl>
+                                <FormControlLabel
+                                    label="隐藏"
+                                    classes={{
+                                    root: this.props.classes.formLabel,
+                                    label: this.props.classes.formLabel
+                                }}
+                                    className={this.props.classes.formControlMargin}
+                                    control={
+                                    <Switch
+                                        classes={{
+                                            root: this.props.classes.switchHeight,
+                                            default: this.props.classes.switchDefault,
+                                        }}
+                                        onChange={(event, checked) => this.setState({ isHidden: checked })}
+                                        checked={this.state.isHidden}
+                                    />
+                                }
                                 />
-                            </FormControl>
+                                <FormControl fullWidth
+                                             className={this.props.classes.formControlMargin}>
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        发布时间
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('time')}
+                                        value={this.state.time}
+                                    />
+                                </FormControl>
+                                <FormControl fullWidth
+                                             className={this.props.classes.formControlMargin}>
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        来源
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('origin')}
+                                        value={this.state.origin}
+                                    />
+                                </FormControl>
+                                <FormControl fullWidth
+                                             className={this.props.classes.formControlMargin}>
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        来源链接
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('link')}
+                                        value={this.state.link}
+                                    />
+                                </FormControl>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                        <Button raised color="primary" style={{marginTop: 34, fontSize: 12, borderRadius: 4}}>
+                            确认提交
+                        </Button>
+                    </form>
                 </Paper>
             </div>
         );
