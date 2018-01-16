db958ad458316553eb728b7619356bbcb7c7808e
tianjign <tfanxi@163.com>
2018-01-16 10:48:04 +0800

    chore: add cms page edit module

---
+++ src/pages/cms/PageEdit.tsx
@@ -0,0 +1,168 @@
+import * as React from 'react';
+import withStyles, { WithStyles } from 'material-ui/styles/withStyles';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import { FormControlLabel, FormControl } from 'material-ui/Form';
+import Input, { InputLabel } from 'material-ui/Input';
+import Switch from 'material-ui/Switch';
+import Button from 'material-ui/Button';
+
+const styles = {
+    root: {
+        'padding': '40px 30px',
+        'margin-bottom': '60px',
+    },
+    container: {
+        display: 'flex',
+        'flex-wrap': 'wrap',
+        'margin': '0',
+    },
+    formLabel: {
+        'flex-direction': 'row-reverse',
+        'margin': '0',
+        'font-size': '16px !important',
+        'color': '#333',
+        'width': '100%',
+    },
+    formLabelFont: {
+        'font-size': '16px',
+    },
+    formControlMargin: {
+        'margin-bottom': '32px',
+    },
+    underline: {
+        '&:before': {
+            background: '#dfdfdf',
+        }
+    },
+    switchHeight: {
+        'height': '20px',
+    },
+    switchDefault: {
+        'height': 'inherit',
+    },
+};
+type State = {
+};
+
+class PageEdit extends React.Component<WithStyles<keyof typeof styles>, State> {
+    state = {
+        name: 'NotAdd',
+        otherName: 'LOGO.png',
+        type: '新闻资讯',
+        isOpen: false,
+    };
+    handleChange = (name: any) => (event: any) => {
+        let val = event.target.value;
+        this.setState({
+            [name]: val,
+        });
+    };
+    render() {
+        return (
+            <div className="top-action-module cms">
+                <p className="crumbs">
+                    CMS / 页面管理 / 全部页面
+                </p>
+                <h4 className="title">编辑</h4>
+                <Paper className={this.props.classes.root}>
+                    <form className={this.props.classes.container} noValidate autoComplete="off">
+                        <Grid container spacing={24}>
+                            <Grid item xs={12} sm={6}>
+                                <FormControl
+                                    fullWidth
+                                    required
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        标题
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                                underline: this.props.classes.underline,
+                                            }}
+                                        onChange={this.handleChange('name')}
+                                        value={this.state.name}
+                                    />
+                                </FormControl>
+                                <div className="editor">
+                                    编辑器插件
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormControl
+                                    fullWidth
+                                    required
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        别名
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                                underline: this.props.classes.underline,
+                                            }}
+                                        onChange={this.handleChange('otherName')}
+                                        value={this.state.otherName}
+                                    />
+                                </FormControl>
+                                <FormControl
+                                    fullWidth
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        分类
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                                underline: this.props.classes.underline,
+                                            }}
+                                        onChange={this.handleChange('type')}
+                                        value={this.state.type}
+                                    />
+                                </FormControl>
+                                <FormControlLabel
+                                    label="开启"
+                                    classes={{
+                                            root: this.props.classes.formLabel,
+                                            label: this.props.classes.formLabel
+                                        }}
+                                    className={this.props.classes.formControlMargin}
+                                    control={
+                                        <Switch
+                                            classes={{
+                                                root: this.props.classes.switchHeight,
+                                                default: this.props.classes.switchDefault,
+                                            }}
+                                            onChange={(event, checked) => this.setState({ isOpen: checked })}
+                                            checked={this.state.isOpen}
+                                        />
+                                    }
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button raised color="primary" style={{marginTop: 34, fontSize: 12, borderRadius: 4}}>
+                            确认提交
+                        </Button>
+                    </form>
+                </Paper>
+            </div>
+        );
+    }
+}
+export default withStyles(styles)(PageEdit);
