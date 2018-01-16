d1c0c26cbd0c74b3881e1382ce09a82ea9f8d92f
zhanghuan <zhhu_123@163.com>
2018-01-16 23:51:53 +0800

    chore(*): merge assets files from project backend-mix

--- src/backend-mix/components/withRoot.tsx
+++ src/backend-mix/components/withRoot.tsx
@@ -6,16 +6,16 @@ import createContext from '../styles/createContext';
 
 // Apply some reset
 const decorate = withStyles(theme => ({
-    '@global': {
-        html: {
-            background: theme.palette.background.default,
-            WebkitFontSmoothing: 'antialiased', // Antialiasing.
-            MozOsxFontSmoothing: 'grayscale', // Antialiasing.
-        },
-        body: {
-            margin: 0,
-        },
+  '@global': {
+    html: {
+      background: theme.palette.background.default,
+      WebkitFontSmoothing: 'antialiased', // Antialiasing.
+      MozOsxFontSmoothing: 'grayscale', // Antialiasing.
     },
+    body: {
+      margin: 0,
+    },
+  },
 }));
 
 const AppWrapper = decorate<{ children: JSX.Element }>(props => props.children);
@@ -23,33 +23,33 @@ const AppWrapper = decorate<{ children: JSX.Element }>(props => props.children);
 const context = createContext();
 
 function withRoot(BaseComponent: React.ComponentType) {
-    class WithRoot extends React.Component {
-        componentDidMount() {
-            // Remove the server-side injected CSS.
-            const jssStyles = document.querySelector('#jss-server-side');
-            if (jssStyles && jssStyles.parentNode) {
-                jssStyles.parentNode.removeChild(jssStyles);
-            }
-        }
-
-        render() {
-            return (
-                <JssProvider registry={ context.sheetsRegistry } jss={ context.jss }>
-                    <MuiThemeProvider theme={ context.theme } sheetsManager={ context.sheetsManager }>
-                        <AppWrapper>
-                            <BaseComponent/>
-                        </AppWrapper>
-                    </MuiThemeProvider>
-                </JssProvider>
-            );
-        }
+  class WithRoot extends React.Component {
+    componentDidMount() {
+      // Remove the server-side injected CSS.
+      const jssStyles = document.querySelector('#jss-server-side');
+      if (jssStyles && jssStyles.parentNode) {
+        jssStyles.parentNode.removeChild(jssStyles);
+      }
     }
 
-    if (process.env.NODE_ENV !== 'production') {
-        (WithRoot as any).displayName = wrapDisplayName(BaseComponent, 'withRoot');
+    render() {
+      return (
+          <JssProvider registry={context.sheetsRegistry} jss={context.jss}>
+              <MuiThemeProvider theme={context.theme} sheetsManager={context.sheetsManager}>
+                  <AppWrapper>
+                      <BaseComponent />
+                  </AppWrapper>
+              </MuiThemeProvider>
+          </JssProvider>
+      );
     }
+  }
 
-    return WithRoot;
+  if (process.env.NODE_ENV !== 'production') {
+    (WithRoot as any).displayName = wrapDisplayName(BaseComponent, 'withRoot');
+  }
+
+  return WithRoot;
 }
 
 export default withRoot;
