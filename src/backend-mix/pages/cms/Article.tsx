ce644d6dd67a6eee1da057d8a377ee62285ef05d
zhanghuan <zhhu_123@163.com>
2018-01-16 23:47:54 +0800

    chore(*): copy files from project backend-mix

---
+++ src/backend-mix/pages/cms/Article.tsx
@@ -0,0 +1,321 @@
+import * as React from 'react';
+import withStyles, { WithStyles } from 'material-ui/styles/withStyles';
+import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+import Paper from 'material-ui/Paper';
+import Checkbox from 'material-ui/Checkbox';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import DeleteIcon from 'material-ui-icons/Delete';
+import ClearIcon from 'material-ui-icons/Clear';
+import ModeEdit from 'material-ui-icons/ModeEdit';
+import Search from 'material-ui-icons/Search';
+import Add from 'material-ui-icons/Add';
+import Cached from 'material-ui-icons/Cached';
+import Table, {
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+} from 'material-ui/Table';
+import Dialog, {
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+} from 'material-ui/Dialog';
+
+const styles = {
+    evenRow: {
+        'background': '#f7f7f7',
+    },
+    menuBtn: {
+        'width': '32px',
+        'height': '32px',
+        'border-radius': '50%',
+        'background-color': '#3f51b5',
+        'color': '#fff',
+        'margin-left': '10px',
+    },
+    btnEdit: {
+        'width': '32px',
+        'height': '32px',
+        'border-radius': '50%',
+        'background-color': '#3f51b5',
+        'color': '#fff',
+        'margin-left': '10px',
+        'box-shadow': '0px 2px 4px 0 rgba(0, 0, 0, 0.3)',
+    },
+    btnDelete: {
+        'width': '32px',
+        'height': '32px',
+        'border-radius': '50%',
+        'background-color': '#fff',
+        'color': '#808080',
+        'margin-left': '10px',
+        'box-shadow': '0px 2px 4px 0 rgba(0, 0, 0, 0.3)',
+    },
+    root: {
+        'padding': '40px 30px',
+    },
+    table: {
+        'border-top': '1px solid rgba(235, 235, 235, 1)',
+        'border-collapse': 'inherit',
+    },
+    tableCell: {
+        'text-align': 'left',
+        'padding': '0',
+    },
+    tableCellStatus: {
+        'text-align': 'left',
+        'padding-left': '0',
+        'padding-right': '0',
+        'width': '40px',
+    },
+};
+type State = {
+    checkedAll: boolean,
+    rowsPerPage: number,
+    currentPage: number,
+    open: boolean,
+    modalId: string,
+    modalName: string,
+};
+
+class Article extends React.Component<WithStyles<keyof typeof styles>, State> {
+    state = {
+        checkedAll: false,
+        rowsPerPage: 2,
+        currentPage: 0,
+        open: false,
+        modalId: '',
+        modalName: '',
+        list: [
+            {
+                id: 1,
+                check: false,
+                name: '标题名称测试标题名称测试标题名称测试标题名称测试',
+                type: '新闻资讯1',
+                time: '2017-12-01 13:20:59',
+            },
+            {
+                id: 2,
+                check: false,
+                name: '标题名称测试标题名称测试标题名称测试标题名称测试',
+                type: '新闻资讯2',
+                time: '2017-12-01 13:20:59',
+            },
+            {
+                id: 3,
+                check: false,
+                name: '标题名称测试标题名称测试标题名称测试标题名称测试',
+                type: '新闻资讯3',
+                time: '2017-12-01 13:20:59',
+            },
+            {
+                id: 4,
+                check: false,
+                name: '标题名称测试标题名称测试标题名称测试标题名称测试',
+                type: '新闻资讯4',
+                time: '2017-12-01 13:20:59',
+            },
+            {
+                id: 5,
+                check: false,
+                name: '标题名称测试标题名称测试标题名称测试标题名称测试',
+                type: '新闻资讯5',
+                time: '2017-12-01 13:20:59',
+            },
+        ],
+    };
+    handleChangeAll = (name: any) => (event: any) => {
+        if (event.target.checked) {
+            this.state.list.map(item => {
+                item.check = true;
+            });
+        } else {
+            this.state.list.map(item => {
+                item.check = false;
+            });
+        }
+        this.setState({
+            [name]: event.target.checked,
+        });
+    };
+    handleChange = (pro: any) => (event: any) => {
+        this.state.checkedAll = true;
+        pro.check = true;
+        if (!event.target.checked) {
+            pro.check = false;
+        }
+        this.state.list.map(item => {
+            if (item.check === false) {
+                this.state.checkedAll = false;
+            }
+        });
+        this.setState({
+            [pro]: event.target.checked,
+        });
+    };
+    handleClickEdit = (pro: any) => {
+        window.console.log(pro);
+    }
+    handleClickRemove = (pro: any) => {
+        this.state.modalName = pro.name;
+        this.state.modalId = pro.id;
+        this.setState({
+            open: true,
+        });
+    };
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+    handleSubmit = () => {
+        this.setState({ open: false });
+    };
+    handlePageClick = (data: any) => {
+        this.setState({ currentPage: data.selected });
+    };
+
+    render() {
+        const { currentPage, rowsPerPage, list } = this.state;
+        return (
+            <div className="top-action-module cms">
+                <p className="crumbs">
+                    CMS / 文章管理
+                </p>
+                <h4 className="title">全部文章</h4>
+                <div className="btn-group">
+                    <IconButton
+                        className={this.props.classes.menuBtn}
+                    >
+                        <Search />
+                    </IconButton>
+                    <IconButton
+                        className={this.props.classes.menuBtn}
+                    >
+                        <DeleteIcon />
+                    </IconButton>
+                    <IconButton
+                        className={this.props.classes.menuBtn}
+                    >
+                        <Add />
+                    </IconButton>
+                    <IconButton
+                        className={this.props.classes.menuBtn}
+                    >
+                        <Cached />
+                    </IconButton>
+                </div>
+                <Paper className={this.props.classes.root}>
+                    <Table className={this.props.classes.table}>
+                        <TableHead className="table-head">
+                            <TableRow>
+                                <TableCell className={this.props.classes.tableCellStatus}>
+                                    <Checkbox
+                                        checked={this.state.checkedAll}
+                                        onChange={this.handleChangeAll('checkedAll')}
+                                        value="checkedAll"
+                                    />
+                                </TableCell>
+                                <TableCell className={this.props.classes.tableCell} numeric>文章名称</TableCell>
+                                <TableCell className={this.props.classes.tableCell} numeric>分类</TableCell>
+                                <TableCell className={this.props.classes.tableCell} numeric>发布时间</TableCell>
+                                <TableCell numeric/>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody className="table-body">
+                            {list.slice(currentPage * rowsPerPage, rowsPerPage * currentPage + rowsPerPage)
+                                .map((n, index) => {
+                                    return (
+                                        <TableRow
+                                            hover
+                                            className={index % 2 === 0 ? this.props.classes.evenRow : ''}
+                                            key={n.id}
+                                        >
+                                            <TableCell
+                                                padding="checkbox"
+                                                className={this.props.classes.tableCellStatus}
+                                            >
+                                                <Checkbox
+                                                    checked={n.check}
+                                                    onChange={this.handleChange(n)}
+                                                    value="n.check"
+                                                />
+                                            </TableCell>
+                                            <TableCell className={this.props.classes.tableCell} numeric>
+                                                {n.name}
+                                            </TableCell>
+                                            <TableCell className={this.props.classes.tableCell} numeric>
+                                                {n.type}
+                                            </TableCell>
+                                            <TableCell className={this.props.classes.tableCell} numeric>
+                                                {n.time}
+                                            </TableCell>
+                                            <TableCell numeric>
+                                                <Link to={'/cms/article/edit/' + n.id}>
+                                                    <IconButton
+                                                        className={this.props.classes.btnEdit}
+                                                    >
+                                                        <ModeEdit />
+                                                    </IconButton>
+                                                </Link>
+                                                <IconButton
+                                                    className={this.props.classes.btnDelete}
+                                                    onClick={() => this.handleClickRemove(n)}
+                                                >
+                                                    <DeleteIcon />
+                                                </IconButton>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                        </TableBody>
+                    </Table>
+                    <div className="table-pagination">
+                        <ReactPaginate
+                            previousLabel={'<'}
+                            nextLabel={'>'}
+                            breakLabel={<a href="javascript:;">...</a>}
+                            breakClassName={'break-me'}
+                            pageCount={list.length / rowsPerPage}
+                            marginPagesDisplayed={2}
+                            pageRangeDisplayed={2}
+                            onPageChange={this.handlePageClick}
+                            containerClassName={'pagination'}
+                            activeClassName={'active'}
+                        />
+                    </div>
+                </Paper>
+                <Dialog
+                    open={this.state.open}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                    className="dialog-content"
+                >
+                    <DialogTitle
+                        id="alert-dialog-title"
+                        className="dialog-title"
+                    >
+                        <IconButton
+                            onClick={this.handleClose}
+                        >
+                            <ClearIcon />
+                        </IconButton>
+                    </DialogTitle>
+                    <DialogContent className="dialog-content">
+                        <h4>确定要删除文章名称"{this.state.modalName}"吗?</h4>
+                    </DialogContent>
+                    <DialogActions className="dialog-actions">
+                        <Button onClick={this.handleClose}>
+                            取消
+                        </Button>
+                        <Button onClick={this.handleSubmit} autoFocus>
+                            确认提交
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+export default withStyles(styles)(Article);
---
+++ src/backend-mix/pages/cms/ArticleEdit.tsx
@@ -0,0 +1,283 @@
+import * as React from 'react';
+import withStyles, { WithStyles } from 'material-ui/styles/withStyles';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import { FormControlLabel, FormControl } from 'material-ui/Form';
+import Input, { InputLabel } from 'material-ui/Input';
+import Switch from 'material-ui/Switch';
+import Button from 'material-ui/Button';
+
+const styles = {
+    root: {
+        'padding': '40px 30px',
+        'margin-bottom': '60px',
+    },
+    container: {
+        display: 'flex',
+        'flex-wrap': 'wrap',
+        'margin': '0',
+    },
+    formLabel: {
+        'flex-direction': 'row-reverse',
+        'margin': '0',
+        'font-size': '16px !important',
+        'color': '#333',
+        'width': '100%',
+    },
+    formLabelFont: {
+        'font-size': '16px',
+    },
+    formControlMargin: {
+        'margin-bottom': '32px',
+    },
+    underline: {
+        '&:before': {
+            background: '#dfdfdf',
+        }
+    },
+    switchHeight: {
+        'height': '20px',
+    },
+    switchDefault: {
+        'height': 'inherit',
+    },
+};
+type State = {
+    webName: string,
+    img: string,
+    type: string,
+    abstract: string,
+    time: string,
+    link: string,
+    origin: string,
+    kind: string,
+    isHidden: boolean,
+};
+
+class ArticleEdit extends React.Component<WithStyles<keyof typeof styles>, State> {
+    state = {
+        webName: 'NotAdd',
+        img: 'LOGO.png',
+        type: '新闻资讯',
+        abstract: '',
+        time: '2016-12-30',
+        link: 'http://',
+        origin: 'www.ibenchu.com',
+        kind: '新闻资讯',
+        isHidden: false,
+    };
+    handleChange = (name: any) => (event: any) => {
+        let val = event.target.value;
+        this.setState({
+            [name]: val,
+        });
+    };
+    render() {
+        return (
+            <div className="top-action-module cms">
+                <p className="crumbs">
+                    CMS / 文章管理 / 全部文章
+                </p>
+                <h4 className="title">编辑</h4>
+                <Paper className={this.props.classes.root}>
+                    <form className={this.props.classes.container} noValidate autoComplete="off">
+                        <Grid container spacing={24}>
+                            <Grid item xs={12} sm={6}>
+                                <FormControl
+                                    fullWidth
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        文章标题
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('webName')}
+                                        value={this.state.webName}
+                                    />
+                                </FormControl>
+                                <div className="editor">
+                                    编辑器插件
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormControl
+                                    fullWidth
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        缩略图
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('img')}
+                                        value={this.state.img}
+                                    />
+                                </FormControl>
+                                <FormControl
+                                    fullWidth
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        分类
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('type')}
+                                        value={this.state.type}
+                                    />
+                                </FormControl>
+                                <FormControl
+                                    fullWidth
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        摘要
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        multiline={true}
+                                        rowsMax="3"
+                                        rows="3"
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        className={this.props.classes.formLabelFont}
+                                        onChange={this.handleChange('abstract')}
+                                        value={this.state.abstract}
+                                    />
+                                </FormControl>
+                                <FormControl
+                                    fullWidth
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        分类
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('kind')}
+                                        value={this.state.kind}
+                                    />
+                                </FormControl>
+                                <FormControlLabel
+                                    label="隐藏"
+                                    classes={{
+                                        root: this.props.classes.formLabel,
+                                        label: this.props.classes.formLabel
+                                    }}
+                                    className={this.props.classes.formControlMargin}
+                                    control={
+                                    <Switch
+                                        classes={{
+                                            root: this.props.classes.switchHeight,
+                                            default: this.props.classes.switchDefault,
+                                        }}
+                                        onChange={(event, checked) => this.setState({ isHidden: checked })}
+                                        checked={this.state.isHidden}
+                                    />
+                                }
+                                />
+                                <FormControl
+                                    fullWidth
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        发布时间
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('time')}
+                                        value={this.state.time}
+                                    />
+                                </FormControl>
+                                <FormControl
+                                    fullWidth
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        来源
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('origin')}
+                                        value={this.state.origin}
+                                    />
+                                </FormControl>
+                                <FormControl
+                                    fullWidth
+                                    className={this.props.classes.formControlMargin}
+                                >
+                                    <InputLabel
+                                        htmlFor="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                    >
+                                        来源链接
+                                    </InputLabel>
+                                    <Input
+                                        id="name-simple"
+                                        className={this.props.classes.formLabelFont}
+                                        classes={{
+                                            underline: this.props.classes.underline,
+                                        }}
+                                        onChange={this.handleChange('link')}
+                                        value={this.state.link}
+                                    />
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+                        <Button raised color="primary" style={{marginTop: 34, fontSize: 12, borderRadius: 4}}>
+                            确认提交
+                        </Button>
+                    </form>
+                </Paper>
+            </div>
+        );
+    }
+}
+export default withStyles(styles)(ArticleEdit);
