5bba29d382f9aa117ca0156354c5c0d56eb6ae24
twilroad <269044570@qq.com>
2018-01-17 12:04:28 +0800

    feat(event): add logger list handler definition for logger module

--- packages/logger/events/handlers/index.js
+++ packages/logger/events/handlers/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const logger_create_handler_1 = require("./logger.create.handler");
+const logger_list_handler_1 = require("./logger.list.handler");
 exports.EventHandlers = [
     logger_create_handler_1.LoggerCreateHandler,
+    logger_list_handler_1.LoggerListHandler,
 ];
--- src/logger/events/handlers/index.ts
+++ src/logger/events/handlers/index.ts
@@ -1,5 +1,7 @@
 import { LoggerCreateHandler } from "./logger.create.handler";
+import { LoggerListHandler } from "./logger.list.handler";
 
 export const EventHandlers = [
     LoggerCreateHandler,
+    LoggerListHandler,
 ];
