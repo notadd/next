f3aa2cfee6d9ee98b5e371b64bcb8fa576c60fd9
tianjign <tfanxi@163.com>
2018-01-19 12:34:43 +0800

    chore(*): add batch delete method

--- src/pages/cms/Page.tsx
+++ src/pages/cms/Page.tsx
@@ -73,6 +73,8 @@ type State = {
     open: boolean,
     modalId: string,
     modalName: string,
+    modalType: number,
+    modalNum: number,
 };
 
 class Page extends React.Component<WithStyles<keyof typeof styles>, State> {
@@ -83,6 +85,8 @@ class Page extends React.Component<WithStyles<keyof typeof styles>, State> {
         open: false,
         modalId: '',
         modalName: '',
+        modalType: 0,
+        modalNum: 0,
         list: [
             {
                 id: 1,
@@ -155,6 +159,19 @@ class Page extends React.Component<WithStyles<keyof typeof styles>, State> {
             open: true,
         });
     };
+    handleBatchRemove = () => {
+        const arr = new Array();
+        Object.keys(this.state.list).forEach(item => {
+            if (this.state.list[item].check) {
+                arr.push(this.state.list[item].check);
+                this.setState({
+                    open: true,
+                    modalType: 1,
+                    modalNum: arr.length,
+                });
+            }
+        });
+    };
     handleClose = () => {
         this.setState({ open: false });
     };
@@ -166,7 +183,7 @@ class Page extends React.Component<WithStyles<keyof typeof styles>, State> {
     };
 
     render() {
-        const { currentPage, rowsPerPage, list } = this.state;
+        const { currentPage, rowsPerPage, list, modalType } = this.state;
         return (
             <div className="top-action-module cms">
                 <p className="crumbs">
@@ -181,6 +198,7 @@ class Page extends React.Component<WithStyles<keyof typeof styles>, State> {
                     </IconButton>
                     <IconButton
                         className={this.props.classes.menuBtn}
+                        onClick={this.handleBatchRemove}
                     >
                         <DeleteIcon />
                     </IconButton>
@@ -290,7 +308,9 @@ class Page extends React.Component<WithStyles<keyof typeof styles>, State> {
                         </IconButton>
                     </DialogTitle>
                     <DialogContent className="dialog-content">
-                        <h4>确定要删除文章名称"{this.state.modalName}"吗?</h4>
+                        {
+                            modalType === 0 ? <h4>确定要删除页面名称"{this.state.modalName}"吗?</h4> :
+                                <h4>确定要删除这"{this.state.modalNum}"个页面吗?</h4>}
                     </DialogContent>
                     <DialogActions className="dialog-actions">
                         <Button onClick={this.handleClose}>
