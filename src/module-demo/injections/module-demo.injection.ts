ccffd3723bc1b5c488cab22be80e0bfa38f84aad
twilroad <269044570@qq.com>
2018-02-09 11:26:51 +0800

    feat(module): add module definition for package @notadd/module-demo

---
+++ src/module-demo/injections/module-demo.injection.ts
@@ -0,0 +1,8 @@
+import { Module } from "@notadd/injection/decorators/module.decorator";
+
+@Module({
+    identification: "module-demo",
+    version: "2.0.0",
+})
+export class ModuleDemoInjection {
+}
