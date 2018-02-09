6b4df3f323077ba12c3165bf1c13607725432673
twilroad <269044570@qq.com>
2018-02-09 11:26:30 +0800

    feat(extension): add extension definition for package @notadd/extension-demo

---
+++ src/extension-demo/injections/extension-demo.injection.ts
@@ -0,0 +1,8 @@
+import { Extension } from "@notadd/injection/decorators/extension.decorator";
+
+@Extension({
+    identification: "extension-demo",
+    version: "2.0.0",
+})
+export class ExtensionDemoInjection {
+}
