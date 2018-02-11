f28a2fc31aac23d20dbca1f3f0a31ba978b781b7
twilroad <269044570@qq.com>
2018-02-11 14:58:40 +0800

    feat(injection): add decorator Dashboard

---
+++ packages/injection/decorators/dashboard.decorator.d.ts
@@ -0,0 +1,2 @@
+import 'reflect-metadata';
+export declare function Dashboard(name: string): MethodDecorator;
---
+++ packages/injection/decorators/dashboard.decorator.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const common_1 = require("@nestjs/common");
+const dashboard_constants_1 = require("../constants/dashboard.constants");
+function Dashboard(name) {
+    return (target, key, descriptor) => {
+        common_1.ReflectMetadata(dashboard_constants_1.DASHBOARD_DATA_METADATA, name)(target, key, descriptor);
+        common_1.ReflectMetadata(dashboard_constants_1.DASHBOARD_NAME_METADATA, name)(target, key, descriptor);
+    };
+}
+exports.Dashboard = Dashboard;
