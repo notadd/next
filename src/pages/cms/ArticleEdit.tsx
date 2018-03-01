4a636ae18cf59834a541d079b833f8441b65d2c5
tianjign <tfanxi@163.com>
2018-03-01 16:52:35 +0800

    chore(*): import Ant Design

--- src/pages/cms/ArticleEdit.tsx
+++ src/pages/cms/ArticleEdit.tsx
@@ -75,7 +75,7 @@ class ArticleEdit extends React.Component<WithStyles<keyof typeof styles>, State
         }
         this.state = {
             webName: 'NotAdd',
-            img: 'LOGO.png',
+            img: '',
             type: '',
             topType: '',
             types: [
