b3f5f390a58680f5ab4c933d5ac95295d78c0a67
twilroad <269044570@qq.com>
2018-03-05 14:35:09 +0800

    fix(*): fix package dependency error

--- typings/misc.d.ts
+++ typings/misc.d.ts
@@ -1,11 +1,9 @@
-declare module 'jss'
 declare module 'jss-preset-default'
 declare module 'react-jss/*'
 declare module 'recompose/*'
 declare module 'react-dnd-html5-backend'
 declare module 'react-dnd-touch-backend'
 declare module 'rc-color-picker'
-declare module 'material-ui-pickers'
 declare module 'react-swipeable-views'
 declare module 'react-swipeable-views-utils'
 declare module 'react-dazzle';
