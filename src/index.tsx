4ad0a120cfc0b0c0a5769beb5482b9f22352e65e
twilroad <269044570@qq.com>
2018-03-12 11:55:39 +0800

    chore(*): remove unnecessary files

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import App from './layouts/App';
-import './assets/css/main.css';
-
-render(
-    <App/>,
-    document.querySelector('#root')
-);
