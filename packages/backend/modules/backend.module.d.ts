f26e4184a49a8ed835b015a07f69013344382a08
twilroad <269044570@qq.com>
2018-04-10 10:16:08 +0800

    refactor(backend): remove usage of webpackExpress

--- packages/backend/modules/backend.module.d.ts
+++ packages/backend/modules/backend.module.d.ts
@@ -1,8 +1,3 @@
-import { MiddlewaresConsumer } from "@nestjs/common";
-import { OnModuleInit } from "@nestjs/common/interfaces/modules";
-export declare class BackendModule implements OnModuleInit {
+export declare class BackendModule {
     private logger;
-    constructor();
-    configure(consumer: MiddlewaresConsumer): void;
-    onModuleInit(): any;
 }
--- packages/backend/modules/backend.module.js
+++ packages/backend/modules/backend.module.js
@@ -5,24 +5,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
-const webpack_server_1 = require("@notadd/core/servers/webpack.server");
 let BackendModule = class BackendModule {
     constructor() {
         this.logger = new common_1.Logger("NotaddApplication", true);
     }
-    configure(consumer) {
-        consumer.apply(webpack_server_1.webpackExpress({})).forRoutes({ path: "/admin", method: common_1.RequestMethod.ALL });
-    }
-    onModuleInit() {
-    }
 };
 BackendModule = __decorate([
-    common_1.Module({}),
-    __metadata("design:paramtypes", [])
+    common_1.Module({})
 ], BackendModule);
 exports.BackendModule = BackendModule;
