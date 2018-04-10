31fd1064dc9f34a266deaf4c63e47c5b8789b8af
twilroad <269044570@qq.com>
2018-04-10 10:14:24 +0800

    refactor(backend): remove usage of webpackExpress

--- packages/core/servers/webpack.server.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="express" />
-import { Request, Response } from "express";
-import { WebpackConfiguration } from "../interfaces/webpack-configuration.interface";
-export declare function webpackExpress(options: WebpackConfiguration): (request: Request, response: Response, next: any) => void;
--- src/backend/modules/backend.module.ts
+++ src/backend/modules/backend.module.ts
@@ -1,20 +1,7 @@
-import { Logger, MiddlewaresConsumer, Module, RequestMethod } from "@nestjs/common";
-import { OnModuleInit } from "@nestjs/common/interfaces/modules";
-import { webpackExpress } from "@notadd/core/servers/webpack.server";
+import { Logger, Module } from "@nestjs/common";
 
 @Module({
 })
-export class BackendModule implements OnModuleInit {
-    private logger: Logger;
-
-    constructor() {
-        this.logger = new Logger("NotaddApplication", true);
-    }
-
-    configure(consumer: MiddlewaresConsumer) {
-        consumer.apply(webpackExpress({})).forRoutes({ path: "/admin", method: RequestMethod.ALL });
-    }
-
-    onModuleInit(): any {
-    }
+export class BackendModule {
+    private logger: Logger = new Logger("NotaddApplication", true);
 }
