e7be77577648720bca87cbd85209f99ec3ddc3d8
twilroad <269044570@qq.com>
2018-05-16 19:09:35 +0800

    chore(*): rebuild packages

--- packages/authentication/strategies/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jwt_strategy_1 = require("./jwt.strategy");
-exports.JwtStrategy = jwt_strategy_1.JwtStrategy;
-
-//# sourceMappingURL=index.js.map
