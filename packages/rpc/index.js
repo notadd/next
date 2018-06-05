c588293ef09cf15696a96d73b07155060fad55a2
twilroad <269044570@qq.com>
2018-06-05 15:50:59 +0800

    chore(*): rebuild package

---
+++ packages/rpc/index.js
@@ -0,0 +1,10 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./decorators"));
+__export(require("./errors"));
+__export(require("./microservices"));
+
+//# sourceMappingURL=index.js.map
